80dd67eaae949870b9ee8120b3b215b3b9d34739
Andrew Kirkpatrick <andrew.kirkpatrick@partnerstack.com>
2020-05-05 17:03:10 -0400

    Fix for environment variable supplied QUEUE_LIST

--- celery_prometheus_exporter.py
+++ celery_prometheus_exporter.py
@@ -362,10 +362,11 @@ def main():  # pragma: no cover
     if opts.queue_list:
 
         # CLI argument will likely be a string nested in a list
+        queue_list = opts.queue_list
         if type(opts.queue_list) == list and len(opts.queue_list) == 1:
             queue_list = bytearray(opts.queue_list.pop().encode('utf-8')).decode('unicode_escape').split(',')
-        else:
-            queue_list = opts.queue_list
+        elif type(opts.queue_list) == str:
+            queue_list = [opts.queue_list]
 
         q = QueueLengthMonitoringThread(app=app, queue_list=queue_list)
         q.daemon = True
