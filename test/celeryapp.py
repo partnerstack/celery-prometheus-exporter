918e17d735f1b6d4fabc1c850cfbb2e5acb1a28a
Neil <nchudleigh@gmail.com>
2020-11-10 17:33:55 -0500

    Remove /app folder

--- test/celeryapp.py
+++ test/celeryapp.py
@@ -17,24 +17,33 @@ if RESULT_BACKEND_URL:
 celery_app.conf.update(
     CELERY_DEFAULT_QUEUE="queue1",
     CELERY_QUEUES=(
-        Queue('queue1', exchange=Exchange('queue1', type='direct'), routing_key='queue1'),
-        Queue('queue2', exchange=Exchange('queue2', type='direct'), routing_key='queue2'),
-        Queue('queue3', exchange=Exchange('queue3', type='direct'), routing_key='queue3'),
+        Queue(
+            "queue1", exchange=Exchange("queue1", type="direct"), routing_key="queue1"
+        ),
+        Queue(
+            "queue2", exchange=Exchange("queue2", type="direct"), routing_key="queue2"
+        ),
+        Queue(
+            "queue3", exchange=Exchange("queue3", type="direct"), routing_key="queue3"
+        ),
     ),
     CELERY_ROUTES={
-        'task1': {'queue': 'queue1', 'routing_key': 'queue1'},
-        'task2': {'queue': 'queue2', 'routing_key': 'queue2'},
-        'task3': {'queue': 'queue3', 'routing_key': 'queue3'},
-    }
+        "task1": {"queue": "queue1", "routing_key": "queue1"},
+        "task2": {"queue": "queue2", "routing_key": "queue2"},
+        "task3": {"queue": "queue3", "routing_key": "queue3"},
+    },
 )
+
 
 @celery_app.task
 def task1():
     time.sleep(20)
 
+
 @celery_app.task
 def task2():
     time.sleep(20)
+
 
 @celery_app.task
 def task3():
